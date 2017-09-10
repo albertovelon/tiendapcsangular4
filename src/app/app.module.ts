f21b56df87b62f0f5bdb176ae6f327af125f0e9d
albertovelon <https://albertovelon@bitbucket.org>
2017-09-10 08:20:51 -0500

    funcionando hasta el 20170909

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 import { NgModule } from '@angular/core';
 
+
+import { ClientesModule } from './clientes/clientes.module';
+import { PedidosModule } from './pedidos/pedidos.module';
+import { IndexComponent } from './clientes/index/index.component';
+import { AddComponent as PedidosAddComponent} from './pedidos/add/add.component';
+
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { CurrentOptionComponent } from './current-option/current-option.component';
+
+//configuracion de rutas, mediante un objeto que se especifica
+const appRoutes: Routes = [
+  { path: "clientes/index", component: IndexComponent },
+  { path: "pedidos/add", component: PedidosAddComponent },
+];
+
+//fin de configuracion de rutas
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CurrentOptionComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ClientesModule,
+    PedidosModule,
+    RouterModule.forRoot(appRoutes),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
